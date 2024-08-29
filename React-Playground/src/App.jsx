0c40d32e49202e2135cdb0fbf6d636b76915b699
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2024-08-29 22:33:01 +0530

    [WebDevelopment:React]: Updated src.

--- React-Playground/src/App.jsx
+++ React-Playground/src/App.jsx
@@ -2,14 +2,20 @@ import './App.css'
 import Card from './Components/Card'
 import Weapon from './Components/Weapon'
 import Song from './Components/Song'
+import UseState01 from './Components/UseState01'
+import UseState02 from './Components/UseState02'
+import UseState03 from './Components/UseState03'
 
 function App() {
   
   return (
     <>
-      <Card />
+      {/* <Card />
       <Weapon />
       <Song />
+      <UseState01 />
+      <UseState02 /> */}
+      <UseState03 />
     </>
   )
 }
